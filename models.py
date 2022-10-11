ab155d7c8ce241d52e0a0953f890a38bb33f80ad
muntan <serdar_tst@hotmail.com>
2022-10-11 17:42:59 +0200

    generator layers

--- models.py
+++ models.py
@@ -13,29 +13,29 @@ class Generator(nn.Module):
             OrderedDict([
                 # Block 1:input is Z, going into a convolution
                 ('ConvTranspose2d_1',
-                 nn.ConvTranspose2d(Z,)),
+                 nn.ConvTranspose2d(Z,512)),
                 ('BatchNorm2d_1', nn.BatchNorm2d()),
                 ('ReLU_1', nn.ReLU()),
 
                 # Block 2: input is (64 * 8) x 4 x 4
                 ('ConvTranspose2d_2',
-                 nn.ConvTranspose2d()),
+                 nn.ConvTranspose2d(512,256)),
                 ('BatchNorm2d_2', nn.BatchNorm2d()),
                 ('ReLU_2', nn.ReLU()),
 
                 # Block 3: input is (64 * 4) x 8 x 8
                 ('ConvTranspose2d_3',
-                 nn.ConvTranspose2d()),
+                 nn.ConvTranspose2d(256,128)),
                 ('BatchNorm2d_3', nn.BatchNorm2d()),
                 ('ReLU_3', nn.ReLU()),
 
                 # Block 4: input is (64 * 2) x 16 x 16
-                ('ConvTranspose2d_4', nn.ConvTranspose2d()),
+                ('ConvTranspose2d_4', nn.ConvTranspose2d(128,64)),
                 ('BatchNorm2d_4', nn.BatchNorm2d()),
                 ('ReLU_4', nn.ReLU()),
 
                 # Block 5: input is (64) x 32 x 32
-                ('ConvTranspose2d_5', nn.ConvTranspose2d()),
+                ('ConvTranspose2d_5', nn.ConvTranspose2d(64)),
                 ('Tanh', nn.Tanh())
                 # Output: output is (3) x 64 x 64
             ])
