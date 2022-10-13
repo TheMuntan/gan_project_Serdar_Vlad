cfb252074b24d0e5183b690fc70954a0922e24ad
muntan <serdar_tst@hotmail.com>
2022-10-13 13:55:10 +0200

    kernel, stride and padding added to gen and disc

--- models.py
+++ models.py
@@ -13,29 +13,29 @@ class Generator(nn.Module):
             OrderedDict([
                 # Block 1:input is Z, going into a convolution
                 ('ConvTranspose2d_1',
-                 nn.ConvTranspose2d(Z,512)),
+                 nn.ConvTranspose2d(Z,512,4,1,0)),
                 ('BatchNorm2d_1', nn.BatchNorm2d()),
                 ('ReLU_1', nn.ReLU()),
 
                 # Block 2: input is (64 * 8) x 4 x 4
                 ('ConvTranspose2d_2',
-                 nn.ConvTranspose2d(512,256)),
+                 nn.ConvTranspose2d(512,256,4,2,1)),
                 ('BatchNorm2d_2', nn.BatchNorm2d()),
                 ('ReLU_2', nn.ReLU()),
 
                 # Block 3: input is (64 * 4) x 8 x 8
                 ('ConvTranspose2d_3',
-                 nn.ConvTranspose2d(256,128)),
+                 nn.ConvTranspose2d(256,128,4,2,1)),
                 ('BatchNorm2d_3', nn.BatchNorm2d()),
                 ('ReLU_3', nn.ReLU()),
 
                 # Block 4: input is (64 * 2) x 16 x 16
-                ('ConvTranspose2d_4', nn.ConvTranspose2d(128,64)),
+                ('ConvTranspose2d_4', nn.ConvTranspose2d(128,64,4,2,1)),
                 ('BatchNorm2d_4', nn.BatchNorm2d()),
                 ('ReLU_4', nn.ReLU()),
 
                 # Block 5: input is (64) x 32 x 32
-                ('ConvTranspose2d_5', nn.ConvTranspose2d(64)),
+                ('ConvTranspose2d_5', nn.ConvTranspose2d(64,3,4,2,1)),
                 ('Tanh', nn.Tanh())
                 # Output: output is (3) x 64 x 64
             ])
@@ -55,26 +55,26 @@ class Discriminator(nn.Module):
         self.main = nn.Sequential(
             OrderedDict([
                 # Block 1: input is (3) x 64 x 64
-                ('Conv2d_1', nn.ConvTranspose2d(3,64)),
+                ('Conv2d_1', nn.ConvTranspose2d(3,64,4,2,1)),
                 ('LeakyReLU_1', nn.LeakyReLU()),
 
                 # Block 2: input is (64) x 32 x 32
-                ('Conv2d_2', nn.ConvTranspose2d(64,128)),
+                ('Conv2d_2', nn.ConvTranspose2d(64,128,4,2,1)),
                 ('BatchNorm2d_2', nn.BatchNorm2d()),
                 ('LeakyReLU_2', nn.LeakyReLU()),
 
                 # Block 3: input is (64*2) x 16 x 16
-                ('Conv2d_3', nn.ConvTranspose2d(128,256)),
+                ('Conv2d_3', nn.ConvTranspose2d(128,256,4,2,1)),
                 ('BatchNorm2d_3', nn.BatchNorm2d()),
                 ('LeakyReLU_3', nn.LeakyReLU()),
 
                 # Block 4: input is (64*4) x 8 x 8
-                ('Conv2d_4', nn.ConvTranspose2d(256,512)),
+                ('Conv2d_4', nn.ConvTranspose2d(256,512,4,2,1)),
                 ('BatchNorm2d_4', nn.BatchNorm2d()),
                 ('LeakyReLU_4', nn.LeakyReLU()),
 
                 # Block 5: input is (64*8) x 4 x 4
-                ('Conv2d_5', nn.ConvTranspose2d(512,)),
+                ('Conv2d_5', nn.ConvTranspose2d(512,1,4,2,1)),
                 ('Sigmoid', nn.Sigmoid()),
                 ('Flatten', nn.Flatten())
                 # Output: 1
